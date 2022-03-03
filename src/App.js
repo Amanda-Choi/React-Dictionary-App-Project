61dc53920dbc8d05f0f8ff19dd7fa81da92a696e
Amanda-Choi <96817093+Amanda-Choi@users.noreply.github.com>
2022-03-02 20:08:46 -0800

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      hello
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Code by Amanda Choi</small>
+      </footer>
     </div>
   );
 }
