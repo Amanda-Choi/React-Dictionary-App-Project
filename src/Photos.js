aaadd365e10acdad3c19793078f220926ff8e1a0
Amanda-Choi <96817093+Amanda-Choi@users.noreply.github.com>
2022-03-09 20:03:49 -0800

    Add more photos

--- src/Photos.js
+++ src/Photos.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Photos.css";
 
-export default function Photo(props) {
+export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <div classname="row">
+        <div className="row">
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <img src={photo.src.landscape} className="img-fluid" />;
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
               </div>
             );
           })}
